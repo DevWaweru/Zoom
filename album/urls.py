8c421ce8a44b943768d3a474215f6152053e566d
Richard Waweru <developer.waweru@gmail.com>
2018-06-10 17:33:46 +0300

    Adding route that opens new single image in new window and show all images in that category

--- album/urls.py
+++ album/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$',views.index, name = 'home')
+    url('^$',views.index, name = 'home'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.single_image, name='art')
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# (?P<category>)/
--- album/views.py
+++ album/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import Image, Location
+from .models import Image, Location, Category
 
 # Create your views here.
 def index(request):
     title = 'Home'
     images = Image.get_all_images()
     locations = Location.objects.all()
-    print(images)
     return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
 
-def single_image(request, image_id):
+def single_image(request, category_name, image_id):
     title = f'{image_id}'
+    # print(image_category)
 
-    return render(request,'single_image.html',{'title':title})
+    image = Image.get_image_by_id(image_id)
+    # Get category name
+    print(category_name)
+    image_category = Image.objects.filter(catergory__photo_category = category_name)
+    return render(request,'single_image.html',{'title':title, 'image':image, 'image_category':image_category})
