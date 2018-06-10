26fe8c29d9076872f4975bdb1791306ef357a9fc
Richard Waweru <developer.waweru@gmail.com>
2018-06-10 09:43:06 +0300

    listing the locations on main-menu

--- album/views.py
+++ album/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
-from .models import Image
+from .models import Image, Location
 
 # Create your views here.
 def index(request):
     title = 'Home'
     images = Image.get_all_images()
+    locations = Location.objects.all()
     print(images)
-    return render(request, 'index.html', {'title':title, 'images':images})
+    return render(request, 'index.html', {'title':title, 'images':images, 'locations':locations})
 
 def single_image(request, image_id):
     title = f'{image_id}'
