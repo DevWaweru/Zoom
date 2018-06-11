c49027c0d17accb9b76f231c240eeb98014fff51
Richard Waweru <developer.waweru@gmail.com>
2018-06-11 10:32:38 +0300

    Adding copy link functionality

--- album/tests.py
+++ album/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Location, Category, Image
+import datetime as dt
 
 # Create your tests here.
+class LocationTestClass(TestCase):
+    # Set Up Method
+    def setUp(self):
+        self.nairobi = Location(photo_location='Nairobi')
+        self.nairobi.save_location()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+    
+    def tearDown(self):
+        self.nairobi.delete_location()
--- static/js/script.js
+++ static/js/script.js
@@ -12,4 +12,17 @@ $(document).ready(function(){
     // $('.image-details').mouseleave(function(){
     //     $('.image-details').removeClass('darken')
     // })
+
+    $('.tool-tip').click(function(){
+        copyURL = document.getElementById('img-copy')
+        copyURL.select();
+        document.execCommand('copy');
+
+        toolTip = document.getElementById('tip');
+        toolTip.innerHTML = 'Copied!'
+    })
+    $('.tool-tip').mouseleave(function(){
+        toolTip = document.getElementById('tip')
+        toolTip.innerHTML = 'Copy Link'
+    })
 })
